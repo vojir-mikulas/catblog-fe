ff45cec296c52228262b0395667145e9c372c0d7
Mikuláš Vojíř <vojir.mikulas@gmail.com>
2022-10-10 20:08:56 +0200

    posts crud

---
+++ src/components/posts/PostItem.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Post from "../../interfaces/Post";
+
+
+interface props{
+    config: Post
+}
+
+const PostItem : React.FC<props> = ({config}) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default PostItem;
